2828dd66fe778bd1b431909f97f0f26ed09dcb56
Yuri Santana <yuri.luana2016@gmail.com>
2023-11-24 17:00:12 -0300

    commit teste

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Cejam Reminder!</Text>
+      <Text style={styles.participants}>
+        <Text style={styles.bold}>EMMY</Text> = Ellen, Maria, Murilo, Yuri
+      </Text>
       <StatusBar style="auto" />
+      <Text style={styles.left}>
+        Para começar, acesse uma branch: {"\n"}git checkout -b seu_nome
+      </Text>
+      <Text style={[styles.bold]}>
+        {"\n"}
+        EXEMPLO:{"\n"} git checkout -b ellen
+      </Text>
     </View>
   );
 }
@@ -13,8 +23,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+    color: "darkblue",
+  },
+  participants: {
+    fontSize: 20,
+    paddingBottom: 16,
+  },
+  left: {
+    textAlign: "left",
+  },
+  bold: {
+    fontWeight: "bold",
   },
 });
